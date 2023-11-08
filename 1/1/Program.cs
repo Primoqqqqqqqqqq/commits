d31bbf76e19c5d6e081c0e3d035483d236195c3f
Loginov Egor <malder174@gmail.com>
2023-11-08 22:24:21 +0500

    за 7.11

---
+++ 1/1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace рекурсивные_ф_и
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[20];
+            int[] a1 = new int[20];
+
+            Random random = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(100);
+                a1[i] = a[i];
+
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+
+            int min = a1[0];
+            int max = a1[0];
+            int minIndex = 0;
+            int maxIndex = 0;
+
+            for (int i = 1; i < a1.Length; i++)
+            {
+                if (a1[i] < min)
+                {
+                    min = a1[i];
+                    minIndex = i;
+                }
+
+                if (a1[i] > max)
+                {
+                    max = a1[i];
+                    maxIndex = i;
+                }
+            }
+
+            int t = a1[minIndex];
+            a1[minIndex] = a1[maxIndex];
+            a1[maxIndex] = t;
+
+            for (int i = 0; i < a1.Length; i++)
+            {
+                Console.Write($"{a1[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2/рекурсивные ф-и/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace рекурсивные_ф_и
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] a = new int[40];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(20)-10;
+            }
+
+            Array.Sort(a);
+            Array.Reverse(a);
+
+            using (StreamWriter writer = new StreamWriter("d:\\F1.txt"))
+            {
+                foreach (int num in a)
+                {
+                    writer.WriteLine(num);
+                }
+            }
+
+            Array.Sort(a);
+
+            using (StreamWriter writer = new StreamWriter("d:\\F2.txt"))
+            {
+                foreach (int num in a)
+                {
+                    writer.WriteLine(num);
+                }
+            }
+
+            Console.WriteLine("ready");
+        }
+    }
+}
